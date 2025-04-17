e505d5cc1d43ae073a387cb1784e8d7b91f7b4a4
Alireza <alirzahosinopoor.1390@gmail.com>
2025-04-17 16:52:53 +0330

    add GPS feature

--- ai/model.py
+++ ai/model.py
@@ -3,31 +3,28 @@ from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Dense, Input
 import numpy as np
 
-# تعریف پارامترها
-INPUT_SIZE = 9   # x, y, z, vx, vy, vz, yaw, pitch, roll
-OUTPUT_SIZE = 3  # delta_yaw, delta_pitch, delta_roll
+# مشخصات ورودی/خروجی
+INPUT_SIZE = 12  # GPS فعلی + GPS هدف + سرعت + جهت
+OUTPUT_SIZE = 3  # delta_yaw, delta_pitch, delta_thrust
 
-# ساخت مدل
-def build_model():
+def build_gps_navigation_model():
     model = Sequential([
         Input(shape=(INPUT_SIZE,)),
-        Dense(64, activation='relu'),
+        Dense(128, activation='relu'),
         Dense(64, activation='relu'),
         Dense(OUTPUT_SIZE, activation='tanh')  # خروجی بین -1 تا 1
     ])
     model.compile(optimizer='adam', loss='mse')
     return model
 
-# مدل رو بسازیم
-model = build_model()
+model = build_gps_navigation_model()
 model.summary()
 
-# داده‌های مجازی برای آموزش تستی
-X_train = np.random.rand(1000, INPUT_SIZE)
-y_train = np.random.rand(1000, OUTPUT_SIZE) * 2 - 1  # خروجی بین -1 تا 1
+# داده‌های تستی مجازی (فعلاً برای آموزش اولیه)
+X_train = np.random.rand(5000, INPUT_SIZE)
+y_train = np.random.rand(5000, OUTPUT_SIZE) * 2 - 1
 
-# آموزش مدل
-model.fit(X_train, y_train, epochs=20, batch_size=32)
+model.fit(X_train, y_train, epochs=30, batch_size=32)
 
 # ذخیره مدل
-model.save("ai/saved_model/aeronavis_model")
+model.save("ai/saved_model/gps_nav_model")
