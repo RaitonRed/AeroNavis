15ed50033dc92e07728c5fa7c17b86a9d37ca8f0
Alireza <alirzahosinopoor.1390@gmail.com>
2025-04-17 17:03:26 +0330

    =/

--- ai/model.py
+++ ai/model.py
@@ -1,6 +1,6 @@
 import tensorflow as tf
-from tensorflow.keras.models import Sequential
-from tensorflow.keras.layers import Dense, Input
+from keras.models import Sequential
+from keras.layers import Dense, Input
 import numpy as np
 
 # مشخصات ورودی/خروجی
